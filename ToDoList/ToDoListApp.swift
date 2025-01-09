c01756ad2b543b4c94dea9d9f61399196e91db7e
Ngoni Katsidzira <ngonik8@gmail.com>
2025-01-09 23:18:19 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ngoni Katsidzira  on 9/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
